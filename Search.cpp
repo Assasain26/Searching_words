7a0de74bd83eb7868d3918e51d6a33710696125b
Assasain26 <sashahr2309@gmail.com>
2023-08-21 15:31:14 +0300

    v 0.2.0

--- Search.cpp
+++ Search.cpp
@@ -1,56 +1,17 @@
 ﻿#include <iostream>
-#include <string>
 #include <fstream>
-
-using namespace std;
-int countWord(string name) {
-    int size = 0;
-    ifstream file(name);
-    while (!file.eof()) {
-        string word;
-        getline(file, word, ' ');
-        size++;
-    }
-    file.close();
-    return size;
-}
-
-string saveDataFile(string name, string* words, int sizeArr) {
-    ifstream file(name);
-    for (int i = 0; i < sizeArr; i++) {
-        getline(file, words[i], ' ');
-    }
-    file.close();
-    return *words;
-}
-
-int findWord(string name, string* words, int n) {
-    int result = 0;
-    string line;
-    ifstream file(name);
-    while (getline(file, line)) {
-        for (int i = 0; i < n; i++) {
-            if ((line).find((words[i])) != string::npos)
-            {
-                result = 1;
-                break;
-            }
-        }
-    }
-    file.close();
-    return result;
-}
+#include <locale>
+#include "Functions.h"
 
 int main()
 {
-    string name, words[50];
-    cin >> name;
-    const int sizeArr = countWord(name);
-    *words = saveDataFile(name, words, sizeArr);
-    for (int i = 0; i < sizeArr; i++) {
-        cout << words[i] << " ";
-    }
-    cout << "\n\n file.txt = " << findWord("file.txt", *&words, sizeArr);
-    cout << "\n\n file02.txt = " << findWord("file02.txt", *&words, sizeArr);
+    setlocale(LC_ALL, "Russian");
+    /* Тестовые данные */
+    LIST* lst1 = NULL;
+    int size_lst1 = countWord("11.txt");
+    lst1 = read_list(lst1, "11.txt", size_lst1);
+    int otvet = findWord("22.txt", lst1, size_lst1);
+    cout << otvet;
+    /* Тестовые данные */
     return 0;
 }
