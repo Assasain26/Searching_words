4d186f70dc96a717f6452525f8cc83ce42c95533
Assasain26 <sashahr2309@gmail.com>
2023-08-22 18:00:07 +0300

    v 0.3.0

--- Search.cpp
+++ Search.cpp
@@ -1,17 +1,52 @@
-﻿#include <iostream>
-#include <fstream>
-#include <locale>
+﻿#include <locale>
 #include "Functions.h"
 
 int main()
 {
     setlocale(LC_ALL, "Russian");
+
+
+    string path_search;
+    cout << "Введите путь поиска: ";
+    cin >> path_search;
+    
+
+    short depth = 0;
+    while ((depth <= 0) || (depth > 5))
+    {
+        cout << "Введите глубину поиска (0 < x < 6): ";
+        cin >> depth;
+        if (depth <= 0)
+        {
+            cout << "Глубина должна быть больше 0!\n\n";
+        }
+        if (depth > 5)
+        {
+            cout << "Глубина должна быть меньше 5!\n\n";
+        }
+    }
+
+
+    string file_words;
+    cout << "Введите файл слов: ";
+    cin >> file_words; // 11.txt
+    if_exist(file_words);
+
+    
+
     /* Тестовые данные */
     LIST* lst1 = NULL;
-    int size_lst1 = countWord("11.txt");
-    lst1 = read_list(lst1, "11.txt", size_lst1);
-    int otvet = findWord("22.txt", lst1, size_lst1);
+    int size_lst1 = countWord(file_words);
+    lst1 = read_list(lst1, file_words, size_lst1);
+    int otvet = findWord(lst1, "22.txt", size_lst1);
     cout << otvet;
     /* Тестовые данные */
+
+
+    string file_output;
+    cout << "Введите файл вывода: ";
+    cin >> file_output; // 33.txt
+    write_list(lst1, file_output, size_lst1);
+
     return 0;
 }
